19ac4418526e9e82acc944212c95f24597fd60d9
LiangLouise <royliang5@gmail.com>
2021-02-15 01:06:00 -0500

    Add about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="page__bd">
+        <div class="weui-panel weui-panel_access">
+            <div class="weui-panel__hd">About</div>
+            <p>This app won't work without:</p>
+            <div class="weui-panel__bd">
+                <div class="weui-media-box weui-flex">
+                    <div class="weui-media-box_text weui-flex__item">
+                        <h4 class="weui-media-box__title">UI Library</h4>
+                        <p class="weui-media-box__desc"><a href="https://github.com/Tencent/weui" target="__blank">weui</a></p>
+                    </div>
+                </div>    
+                <div class="weui-media-box weui-flex">
+                  <div class="weui-media-box_text weui-flex__item">
+                        <h4 class="weui-media-box__title">Frame Work</h4>
+                        <p class="weui-media-box__desc"><a href="https://vuejs.org/index.html" target="__blank">VueJS</a></p>
+                  </div>
+                </div>  
+            </div>
+        </div>
+    </div>
   </div>
 </template>
